628ce9d6e18aadaed699bf7971ac799422e86828
Gabriel Ferns <gabeferns@meta.com>
2025-04-02 15:11:04 -0700

    add tests

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -782,36 +782,37 @@ class BaseSchedulerNode:
     @cache_on_self
     def estimate_flops(self) -> int | None:
         op = kernel_name_to_op.get(getattr(self.node, "python_kernel_name", ""), None)
-        # extern kernel is needed for kernel.node.inputs/fx_node
-        if not isinstance(self, ExternKernel) or op is None:
+
+        if not isinstance(self.node, ExternKernel) or op is None:
             return None
+
+        # extern kernel is needed for kernel.node.inputs/fx_node
 
         # mypy isn't smart enough to infer from ExternKernel that self.node.inputs
         # and self.node.fx_node exists
-        kern: ExternKernel = self
+        kern: ExternKernel = self.node
 
-        if kern.node is None:
-            return None
-        if any(len(free_unbacked_symbols(n.get_numel())) > 0 for n in kern.node.inputs):
+        if any(len(free_unbacked_symbols(n.get_numel())) > 0 for n in kern.inputs):
             # Tensor has unbacked symints, we don't know how to estimate
             # runtime for that today
             return None
 
+        # NOTE if we want to expand this to more generic nodes, we need to associate
+        # the ir node with fx_node
         with (
             FlopCounterMode(display=False) as flop_counter_mode,
-            V.set_current_node(kern.node.fx_node),  # type ignore[attr-defined]
+            V.set_current_node(kern.fx_node),  # type ignore[attr-defined]
         ):
             from .ir import ir_node_to_tensor
 
             fake_inputs = [
                 ir_node_to_tensor(input, guard_shape=False)
-                for input in kern.node.inputs  # type: ignore[attr-defined]
+                for input in kern.inputs  # type: ignore[attr-defined]
             ]
-            cls = kern.node.__class__
-            cls.process_kernel(op, *fake_inputs, **kern.node.kwargs)
+            cls = kern.__class__
+            cls.process_kernel(op, *fake_inputs, **kern.kwargs)
 
             return flop_counter_mode.get_total_flops()
-        return None
 
     @cache_on_self
     def get_estimated_runtime(self) -> float:
