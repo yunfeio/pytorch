3bb1ffa95b0893c497ea36efa4d5b1da82405ccc
Andy Lugo <Andy.LugoReyes@amd.com>
2025-04-09 21:57:07 +0000

    Large kernel (pre-test, post-compile)

--- aten/src/ATen/native/hip/ck_gemm/ck_gemm_kernels/ck_kernel_collection.h
+++ aten/src/ATen/native/hip/ck_gemm/ck_gemm_kernels/ck_kernel_collection.h
@@ -8,8 +8,17 @@ namespace at::native {
  * ck_gemm_kernel_AdtypeBdtypeOUTdtyp_BLOCKSIZE_MBLOCKxNBLOCKxKBLOCK_MPERXDL_NPERXDL_MPERWAVExNPERWAVE_<ABLOCK_CLUSTER_LENS>_<BBLOCK_CLUSTER_LENS>_<BLOCK_CLUSTER_LENS>_CDE_SCALAR_VEC_Intrawave_v3
  */
 
+// Small
 void
 ck_gemm_kernel_bf16bf16bf16_256_128x128x64_32x32_2x2_8x32x1_8x32x1_1x32x1x8_4_Intrawave_v3(
+        CUDABLAS_GEMM_ARGTYPES(at::BFloat16),
+        bool use_padding);
+
+// Medium
+
+// Large
+void
+ck_gemm_kernel_bf16bf16bf16_256_256x224x64_16x16_8x7_8x32x1_8x32x1_1x32x1x8_4_Intrawave_v3(
         CUDABLAS_GEMM_ARGTYPES(at::BFloat16),
         bool use_padding);
 
