199409af7851cf03c9ecdf92116d5ffc298a5cfd
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-04-24 09:50:26 -0300

    Add CPython exception tests
    * test_baseexception.py
    * test_exceptions.py
    * test_exception_group.py
    * test_except_star.py
    * test_exception_variations.py
    * test_raise.py
    * test_sys.py
    
    ghstack-source-id: d7184833e4d9e345cc4dc04eb3389887ce629ede
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/150789

--- torch/_dynamo/variables/builtin.py
+++ torch/_dynamo/variables/builtin.py
@@ -793,8 +793,8 @@ class BuiltinVariable(VariableTracker):
             )
 
         if inspect.isclass(fn) and (
-            issubclass(fn, Exception)
-            # GeneratorExit doens't inherit from Exception
+            issubclass(fn, BaseException)
+            # GeneratorExit doesn't inherit from Exception
             # >>> issubclass(GeneratorExit, Exception)
             # False
             or fn is GeneratorExit
@@ -803,11 +803,7 @@ class BuiltinVariable(VariableTracker):
             def create_exception_class_object(
                 tx: "InstructionTranslator", args, kwargs
             ):
-                if fn is AssertionError and not all(
-                    isinstance(x, variables.ConstantVariable)
-                    and isinstance(x.value, str)
-                    for x in args
-                ):
+                if fn is AssertionError and not check_constant_args(args, kwargs):
                     unimplemented_v2(
                         gb_type="assert with non-string message",
                         context=str(args),
@@ -900,12 +896,8 @@ class BuiltinVariable(VariableTracker):
                             *[x.as_python_constant() for x in args],
                         )
                     except Exception as exc:
-                        unimplemented_v2(
-                            gb_type="constant fold exception",
-                            context=f"attempted to run function {fn} with arguments {args}",
-                            explanation="Encountered exception when attempting to constant fold.",
-                            hints=[*graph_break_hints.DYNAMO_BUG],
-                            from_exc=exc,
+                        raise_observed_exception(
+                            type(exc), tx, args=[ConstantVariable(a) for a in exc.args]
                         )
                     return VariableTracker.build(tx, res)
 
@@ -922,12 +914,10 @@ class BuiltinVariable(VariableTracker):
                                 },
                             )
                         except Exception as exc:
-                            unimplemented_v2(
-                                gb_type="constant fold exception",
-                                context=f"attempted to run function {fn} with arguments {args} {kwargs}",
-                                explanation="Encountered exception when attempting to constant fold.",
-                                hints=[*graph_break_hints.DYNAMO_BUG],
-                                from_exc=exc,
+                            raise_observed_exception(
+                                type(exc),
+                                tx,
+                                args=[ConstantVariable(a) for a in exc.args],
                             )
                         return VariableTracker.build(tx, res)
 
@@ -1316,7 +1306,7 @@ class BuiltinVariable(VariableTracker):
             if len(arg.args) == 0:
                 value = f"{arg.exc_type}"
             else:
-                value = ", ".join(a.as_python_constant() for a in arg.args)
+                value = ", ".join(str(a.as_python_constant()) for a in arg.args)
             return variables.ConstantVariable.create(value=value)
 
     def _call_min_max(self, tx: "InstructionTranslator", *args):
@@ -1983,7 +1973,6 @@ class BuiltinVariable(VariableTracker):
                     "assertNotWarns",
                     "assertWarnsRegex",
                     "assertDictEqual",
-                    "assertSequenceEqual",
                     "assertWarns",
                 )
             ):
